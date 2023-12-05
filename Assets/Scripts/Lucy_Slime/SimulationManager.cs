402e2366251a8c8df0f39c22f414934c7ac0685e
Lucia <luciaguent@gmail.com>
2023-12-05 20:43:35 +0100

    i did so many things
    uhh its a lot

--- Assets/Scripts/Lucy_Slime/SimulationManager.cs
+++ Assets/Scripts/Lucy_Slime/SimulationManager.cs
@@ -1,12 +1,17 @@
 using ComputeShaderUtility;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
+using System.Runtime.InteropServices;
 
 public class SimulationManager : MonoBehaviour
 {
+    private const int updateKernel = 0;
+    private const int colorKernel = 1;
+
     [SerializeField] private RenderTexture displayTexture;
+    [SerializeField] private RenderTexture trailMap;
+    [SerializeField] private RenderTexture diffuseMap;
+    [SerializeField] private RenderTexture foodMap;
 
     [Header("Simulation Shader")]
     [SerializeField] private ComputeShader simulationShader;
@@ -22,37 +27,147 @@ public class SimulationManager : MonoBehaviour
     [Header("Slime Settings")]
     [SerializeField] private int agentsAmount = 100;
     [SerializeField] private float decayRate = 0.5f;
+    [SerializeField] private float moveSpeed = 0.5f;
+    [SerializeField] private float turnSpeed = 0.5f;
+    [SerializeField] private Gradient possibleSpeciesGradient;
+    [SerializeField] private int speciesAmount = 1;
+
+    [SerializeField] private float sensorAngle = Mathf.PI;
+    [SerializeField] private float sensorOffset = 5f;
+
+    private ComputeBuffer settingsBuffer;
+
+    public struct SpeciesSettings
+    {
+        public float moveSpeed;
+        public float turnSpeed;
+        public Color color;
+
+        public float sensorAngle;
+        public float sensorOffset;
+    }
+
+    private SpeciesSettings[] speciesSettings;
+
+    public struct Agent
+    {
+        public int species;
+
+        public Vector2 position;
+        public float angle;
+    }
+
+    private Agent[] agents;
 
     // Start is called before the first frame update
     void Start()
     {
-        shaderSetup();
+        //createRenderTexture(ref displayTexture, width, height, filterMode, format);
+        createRenderTexture(ref trailMap, width, height, filterMode, format);
+        createRenderTexture(ref diffuseMap, width, height, filterMode, format);
 
-        createRenderTexture(ref displayTexture, width, height, filterMode, format);
+        transform.GetComponentInChildren<MeshRenderer>().material.mainTexture = trailMap;
 
-        transform.GetComponentInChildren<MeshRenderer>().material.mainTexture = displayTexture;
+        setupSpecies();
+        spawnAgents();
 
-        Vector3Int threadGroupSizes = getThreadGroupSizes(simulationShader, 0);
-        int numGroupsX = Mathf.CeilToInt(width / (float)threadGroupSizes.x);
-        int numGroupsY = Mathf.CeilToInt(height / (float)threadGroupSizes.y);
-
-        simulationShader.Dispatch(0, numGroupsX, numGroupsY, 1);
-    }
-
-    private void shaderSetup()
-    {
-        simulationShader.SetTexture(0, "ColorMap", displayTexture);
-        simulationShader.SetInt("width", width);
-        simulationShader.SetInt("height", height);
-
-        simulationShader.SetInt("agentsAmount", agentsAmount);
-        simulationShader.SetFloat("decayRate", decayRate);
+        setupShader();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void FixedUpdate()
+    {
+        simulationShader.SetFloat("deltaTime", Time.deltaTime);
+
+        simulationShader.SetBuffer(updateKernel, "speciesSettings", settingsBuffer);
+        simulationShader.SetBuffer(colorKernel, "speciesSettings", settingsBuffer);
+
+        dispatch(updateKernel);
+        dispatch(colorKernel);
+    }
+
+    private void dispatch(int kernelID)
+    {
+        Vector3Int threadGroupSizes = getThreadGroupSizes(simulationShader, kernelID);
+        int numGroupsX = Mathf.CeilToInt(width / (float)threadGroupSizes.x);
+        int numGroupsY = Mathf.CeilToInt(height / (float)threadGroupSizes.y);
+
+        simulationShader.Dispatch(kernelID, numGroupsX, numGroupsY, 1);
+    }
+
+    private void spawnAgents()
+    {
+        agents = new Agent[agentsAmount];
+
+        for (int i = 0; i < agentsAmount; i++)
+        {
+            Vector2 position = new Vector2(width / 2f, height / 2f);
+            float angle = Random.value * Mathf.PI * 2;
+
+            agents[i] = new Agent() { 
+                species = Random.Range(0, speciesAmount),
+
+                position = position, 
+                angle = angle
+            };
+        }
+
+        setupShaderBuffer<Agent>(simulationShader, agents, updateKernel, "agents");
+    }
+
+    private void setupShader()
+    {
+        simulationShader.SetTexture(updateKernel, "trailMap", trailMap);
+        simulationShader.SetTexture(colorKernel, "trailMap", trailMap);
+        simulationShader.SetTexture(updateKernel, "diffuseMap", diffuseMap);
+        simulationShader.SetTexture(colorKernel, "diffuseMap", diffuseMap);
+        simulationShader.SetInt("width", width);
+        simulationShader.SetInt("height", height);
+
+        simulationShader.SetInt("agentsAmount", agentsAmount);
+        simulationShader.SetFloat("decayRate", decayRate);
+        simulationShader.SetFloat("moveSpeed", moveSpeed);
+    }
+
+    private void setupSpecies()
+    {
+        speciesSettings = new SpeciesSettings[speciesAmount];
+
+        for(int i = 0; i < speciesAmount; i++)
+        {
+            speciesSettings[i] = new SpeciesSettings()
+            {
+                moveSpeed = this.moveSpeed,
+                turnSpeed = this.turnSpeed,
+                color = possibleSpeciesGradient.Evaluate(Random.value),
+                sensorAngle = this.sensorAngle,
+                sensorOffset = this.sensorOffset
+            };
+            Debug.Log(speciesSettings[i].color);
+        }
+
+        settingsBuffer = createShaderBuffer<SpeciesSettings>(speciesSettings);
+        simulationShader.SetBuffer(updateKernel, "speciesSettings", settingsBuffer);
+        simulationShader.SetBuffer(colorKernel, "speciesSettings", settingsBuffer);
+    }
+
+    public static ComputeBuffer createShaderBuffer<T>(T[] bufferData)
+    {
+        ComputeBuffer computeBuffer = new ComputeBuffer(bufferData.Length, Marshal.SizeOf(typeof(T)));
+        computeBuffer.SetData(bufferData);
+        return computeBuffer;
+    }
+
+    public static void setupShaderBuffer<T>(ComputeShader computeShader, T[] bufferData, int kernel, string bufferName)
+    {
+        ComputeBuffer computeBuffer = new ComputeBuffer(bufferData.Length, Marshal.SizeOf(typeof(T)));
+        computeBuffer.SetData(bufferData);
+        computeShader.SetBuffer(kernel, bufferName, computeBuffer);
     }
 
     public static Vector3Int getThreadGroupSizes(ComputeShader compute, int kernelIndex = 0)
